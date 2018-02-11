156b367c0dd7d3f714b1220d569bb26b3f5ce91e
a-pavlov <dkfsoft@gmail.com>
2018-02-12 01:16:30 +0500

    Move to Java 1.8, fix scraping for multiple users

--- src/main/java/org/fantlab/Main.java
+++ src/main/java/org/fantlab/Main.java
@@ -1,19 +1,33 @@
 package org.fantlab;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.*;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * Created by apavlov on 08.02.18.
  */
 @Slf4j
 public class Main {
+
+    @AllArgsConstructor
+    @Getter
+    private static class Tuple<X, Y> {
+        private final X first;
+        private final Y second;
+    }
 
     public static void main(String args[]) throws IOException {
         log.info("Hello");
@@ -36,25 +50,67 @@ public class Main {
             WebScraper webSrcapper = new WebScraper();
             webSrcapper.openFLSite();
             webSrcapper.login(username, password);
-            List<WebElement> links = webSrcapper.openHLinks();
+            webSrcapper.openHLinks();
+            List<Tuple<String, String>> links = webSrcapper.openHLinks()
+                    .stream()
+                    .map(x -> new Tuple<String, String>(x.getText(), x.getAttribute("href")))
+                    .collect(Collectors.toList());
 
-            for(WebElement link: links) {
-                String user = FLUtil.link2Name(link.getAttribute("href"));
+            int i = 3;
+            for(Tuple link: links) {
+                String user = FLUtil.link2Name((String)link.getSecond());
                 log.info("user {} id {} link {}"
-                        , link.getText()
+                        , link.getFirst()
                         , user
-                        , link.getAttribute("href"));
-                List<WebScraper.Mark> marks = webSrcapper.getUserMarks(link.getAttribute("href"));
+                        , link.getSecond());
+                List<WebScraper.Mark> marks = webSrcapper.getUserMarks((String) link.getSecond());
                 for(final WebScraper.Mark m: marks) {
-                    log.info("add mark {}", m);
+                    //log.info("add mark {}", m);
                     accum.addMark(user, FLUtil.link2Name(m.getUrl()), m.getValue());
                 }
 
-                break;
+                if (i-- == 0) break;
             }
 
             webSrcapper.saveScreenshot();
             webSrcapper.closeBrowser();
+            String fileMarks = prop.getProperty("marks_file");
+            String fileUsers = prop.getProperty("users_file");
+            String fileBooks = prop.getProperty("books_file");
+
+            if (fileMarks != null) {
+                try (PrintWriter writer = new PrintWriter(fileMarks)) {
+                    for (List<Integer> marks : accum.getMarks()) {
+                        writer.write(marks.stream().map(x -> x.toString()).collect(Collectors.joining(",")));
+                        writer.write('\n');
+                    }
+                } catch(IOException e) {
+                    log.error("I/O error on write marks {}", e);
+                }
+            }
+
+            if (fileUsers != null) {
+                try(PrintWriter writer = new PrintWriter(fileUsers)) {
+                    Iterator<String> itr = accum.getUsers().iterator();
+                    int index = 0;
+                    while(itr.hasNext()) {
+                        writer.write(Integer.toString(index++) + "," + itr.next() + "\n");
+                    }
+                } catch(IOException e) {
+                    log.error("I/O error on write users {}", e);
+                }
+            }
+
+            if (fileBooks != null) {
+                try(PrintWriter writer = new PrintWriter(fileBooks)) {
+                    for(Map.Entry<String, Integer> entry: accum.getBooks().entrySet()) {
+                        writer.write(entry.getValue().toString() + "," + entry.getKey() + "\n");
+                    }
+                } catch(IOException e) {
+                    log.error("I/O error on write books {}", e);
+                }
+            }
+
         } catch(FLException e) {
             log.error("FL exception: {}", e.getMessage());
         } catch(IOException e) {
