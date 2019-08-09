ab1bf3bea16e0a2666194d318217f7fc077f8731
a-pavlov <dkfsoft@gmail.com>
2019-08-09 20:46:29 +0500

    Add async works requests and filtering by genre - fantastic for now

--- mahout/src/main/java/org/dkfsoft/FLMahout.java
+++ mahout/src/main/java/org/dkfsoft/FLMahout.java
@@ -22,19 +22,28 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
+import rx.Observable;
+import rx.schedulers.Schedulers;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
+import static okhttp3.logging.HttpLoggingInterceptor.Level.NONE;
 
 /**
  * Created by apavlov on 10.03.18.
  */
 public class FLMahout {
+
+    private static ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public static void main(String args[]) throws IOException, TasteException {
 
@@ -59,24 +68,45 @@ public class FLMahout {
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(JacksonConverterFactory.create(objectMapper))
                 .client(new OkHttpClient.Builder()
-                        .addInterceptor(new HttpLoggingInterceptor().setLevel(BODY))
+                        .addInterceptor(new HttpLoggingInterceptor().setLevel(NONE))
                         .connectionPool(new ConnectionPool(0,10,TimeUnit.SECONDS)).build())
                         .build().create(FantlabApiClient.class);
 
-        Response<Work> work = fantlabApiClient.getWork("work/1958/extended").toBlocking().first();
+        //Response<Work> work = fantlabApiClient.getWork("work/1958/extended").toBlocking().first();
 
 
         FileDataModel dataModel = new FileDataModel(new File(args[0]));
         UserSimilarity userSimilarity = new PearsonCorrelationSimilarity(dataModel);
 
-        UserNeighborhood neighborhood = new NearestNUserNeighborhood(100, userSimilarity, dataModel);
+        UserNeighborhood neighborhood = new NearestNUserNeighborhood(500, userSimilarity, dataModel);
         Recommender recommender = new GenericUserBasedRecommender(dataModel, neighborhood, userSimilarity);
         //User user = dataModel.getUser(userId);
         //System.out.println("-----");
         //System.out.println("User: " + user);
         //org.dkfsoft.TasteUtils.printPreferences(user, handler.map);
-        List<RecommendedItem> recommendations =
-                recommender.recommend(userId, 50);
+        List<RecommendedItem> recommendations = recommender.recommend(userId, 200);
+        Map<Long, Float> recommendDict = recommendations.stream().collect(Collectors.toMap(RecommendedItem::getItemID, RecommendedItem::getValue));
+
+        List<Work> works = Observable.from(recommendations)
+                .flatMap(x -> Observable.just(x)
+                    .subscribeOn(Schedulers.from(executorService))
+                .flatMap(item -> fantlabApiClient.getWork("work/" + Long.toString(item.getItemID()) + "/extended").onErrorReturn((e) -> null)))
+                .toList()
+                .toBlocking()
+                .first()
+                .stream()
+                .filter(x -> x.isSuccessful())
+                .map(x -> x.body())
+                .filter(x -> x.isFantastic())
+                .collect(Collectors.toList());
+
+        for(Work work: works) {
+            System.out.println("Doc Id: " + "https://fantlab.ru/work" + Long.toString(work.getWork_id()) + " Title: " + work.getTitle() + " Score: " + recommendDict.get(work.getWork_id()));
+            System.out.println("details: " + work.getWork_description());
+        }
+
+        executorService.shutdown();
+
         TasteUtils.printRecs(recommendations, null);
     }
 }
--- mahout/src/main/java/org/dkfsoft/model/Work.java
+++ mahout/src/main/java/org/dkfsoft/model/Work.java
@@ -1,13 +1,23 @@
 package org.dkfsoft.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(exclude = "title,work_description,work_name,work_type,work_type_id,work_year,classificatory")
 public class Work {
-    String title;
-    String work_description;
-    String work_name;
-    String work_type;
+    Long    work_id;
+    String  title;
+    String  work_description;
+    String  work_name;
+    String  work_type;
     Integer work_type_id;
+    Integer work_year;
     Classificatory classificatory;
+
+    public boolean isFantastic() {
+        return classificatory != null &&
+                classificatory.getGenre_group() != null &&
+                classificatory.getGenre_group().stream().flatMap(x -> x.genre.stream().map(y -> y.genre_id)).anyMatch(x -> x.equals(1));
+    }
 }
